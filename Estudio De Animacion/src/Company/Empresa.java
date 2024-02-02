caeb7bd141d8b70253fab8e33ad71ea4c3ce9ce8
José Gómez <gomez.jose@correo.unimet.edu.ve>
2024-02-02 15:42:25 -0400

    Empresa

--- Estudio De Animacion/src/Company/Empresa.java
+++ Estudio De Animacion/src/Company/Empresa.java
@@ -11,9 +11,23 @@ import java.util.concurrent.Semaphore;
  * @author danieldangelo
  */
 public class Empresa {
+//        public int dayDuration;
+//        public int screenWriters;
+//        public int designers;
+//        public int animators;
+//        public int actors;
+//        public int plotTwists;
+//        public int assemblers;
+//        public int delivary;
+//        Semaphore mainMutex = new Semaphore(1);
+//        Drive drive = new Drive();
+//        
+//        public Empresa(int dayDuration, int screenWriters, int designers, int animators, int actors, int plotTwists, int assemblers, int delivery){
+//            
+//        }
     
-    public Drive drive;
-    public Semaphore semaphore;
+//    public Drive drive;
+//    public Semaphore semaphore;
     //guionistas
     //diseñadores de escenarios
     //animador de personajes
